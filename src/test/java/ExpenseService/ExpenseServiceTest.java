900edbc250e17224107a8ca2fd5e126cdb4ffab4
Teng YANG <teng.yang@oocl.com>
2019-07-15 18:21:15 +0800

    Test: Add test case for external project with other project name

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -47,8 +47,13 @@ class ExpenseServiceTest {
     @Test
     void should_return_other_expense_type_if_project_is_external_and_has_other_name() throws UnexpectedProjectTypeException {
         // given
+        ProjectType projectType = ProjectType.EXTERNAL;
+        String projectName = "Project C";
+        Project project = new Project(projectType, projectName);
         // when
+        ExpenseType expenseType = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
         // then
+        Assertions.assertEquals(ExpenseType.OTHER_EXPENSE, expenseType);
     }
 
     @Test
