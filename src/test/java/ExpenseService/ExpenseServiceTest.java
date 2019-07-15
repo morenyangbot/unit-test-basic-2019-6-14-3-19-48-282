9d81842f2abb29d91a09484c1b768fd376d03993
Teng YANG <teng.yang@oocl.com>
2019-07-15 18:17:52 +0800

    Test: Add test case for internal project type

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -4,17 +4,20 @@ import ExpenseService.Exception.UnexpectedProjectTypeException;
 import ExpenseService.Expense.ExpenseType;
 import ExpenseService.Project.Project;
 import ExpenseService.Project.ProjectType;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
 
 class ExpenseServiceTest {
     @Test
     void should_return_internal_expense_type_if_project_is_internal() throws UnexpectedProjectTypeException {
         // given
+        ProjectType projectType = ProjectType.INTERNAL;
+        String projectName = "ProjectName";
+        Project project = new Project(projectType, projectName);
         // when
+        ExpenseType expenseType = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
         // then
+        Assertions.assertEquals(ExpenseType.INTERNAL_PROJECT_EXPENSE, expenseType);
     }
 
     @Test
